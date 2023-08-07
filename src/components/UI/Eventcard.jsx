b47cef5ead5ef23356f7c15dcdf8e85ae654e3cd
McLizzerson <liesbeth.fr@gmail.com>
2023-08-07 14:56:17 +0200

    Deconstruct date and time

--- src/components/UI/Eventcard.jsx
+++ src/components/UI/Eventcard.jsx
@@ -1,13 +1,21 @@
 import { Card, Heading, Image } from "@chakra-ui/react";
 
 export const EventCard = ({ event }) => {
+  const date = event.startTime.split("T")[0];
+  const start = event.startTime.split("T")[1].slice(0, 5);
+  const end = event.endTime.split("T")[1].slice(0, 5);
   return (
-    <Card>
+    <Card variant="filled" padding={1}>
       <Heading>{event.title}</Heading>
-      <p>"{event.description}"</p>
+      <p>{date}</p>
       <Image src={event.image} alt={event.title} boxSize="xs" />
-      <p>Start time: {event.startTime}</p>
-      <p>End time: {event.endTime}</p>
+      <p>"{event.description}"</p>
+      <br />
+
+      <p>Start time: {start}</p>
+      <p>End time: {end}</p>
+      <br />
+
       <p>Categories: fix category id</p>
     </Card>
   );
