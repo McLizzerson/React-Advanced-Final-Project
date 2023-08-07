19efa5fcb14d6d7d2c4e0b0ef7cbc41097a2f364
McLizzerson <liesbeth.fr@gmail.com>
2023-08-07 11:33:45 +0200

    Add basic text to pages

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { Heading } from "@chakra-ui/react";
 
 export const EventPage = () => {
-  return <Heading>Event</Heading>;
+  return (
+    <>
+      <Heading>Event</Heading>
+      <div>
+        <ul>
+          <li>Name: name of event</li>
+          <li>Description: what's it all about</li>
+          <li>Nice image </li>
+          <li>Time: start - end</li>
+          <li>Categories: fun?</li>
+          <li>Made by: name and image</li>
+        </ul>
+        <br />
+
+        <button>Edit event</button>
+        <br />
+        <button>Delete event</button>
+      </div>
+    </>
+  );
 };
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React from "react";
+import { Heading } from "@chakra-ui/react";
 
 export const EventsPage = () => {
-  return <Heading>List of events</Heading>;
+  return (
+    <>
+      <Heading>List of events</Heading>
+
+      <div>
+        <p>generate event cards using map?</p>
+        <ul>
+          <li>Even title</li>
+          <li>Description</li>
+          <li>Image</li>
+          <li>Time: start - end</li>
+          <li>Categories: fun fun fun</li>
+        </ul>
+        <br />
+
+        <button>Add event</button>
+
+        <input type="text" placeholder="search here for name" />
+        <input type="text" placeholder="search here per category" />
+      </div>
+    </>
+  );
 };
