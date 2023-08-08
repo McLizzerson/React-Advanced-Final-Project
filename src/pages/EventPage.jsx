12e2d2b744bee476cf852c5ece972b25c7d71f63
McLizzerson <liesbeth.fr@gmail.com>
2023-08-08 12:33:59 +0200

    Add context to eventpage for categories

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -8,7 +8,10 @@ import {
   CardHeader,
   Button,
 } from "@chakra-ui/react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
+import { EventsContext } from "../Context";
+import { useContext } from "react";
+import { UserCard } from "../components/UI/UserCard";
 
 export const loader = async ({ params }) => {
   const event = await fetch(`http://localhost:3000/events/${params.eventId}`);
@@ -19,10 +22,20 @@ export const loader = async ({ params }) => {
 
 export const EventPage = () => {
   const { event } = useLoaderData();
+  const { categories } = useContext(EventsContext);
 
   const date = event.startTime.split("T")[0];
   const start = event.startTime.split("T")[1].slice(0, 5);
   const end = event.endTime.split("T")[1].slice(0, 5);
+
+  let categoryList = [];
+  event.categoryIds.map((id) => {
+    categories.map((category) => {
+      if (category.id === id) {
+        categoryList.push(category.name);
+      }
+    });
+  });
 
   return (
     <Card variant="filled" padding={1} size="md" maxW="md" align="center">
@@ -30,6 +43,7 @@ export const EventPage = () => {
         <Heading>{event.title}</Heading>
         <p>{date}</p>
       </CardHeader>
+
       <CardBody>
         <Image src={event.image} alt={event.title} boxSize="xs" />
         <p>"{event.description}"</p>
@@ -38,12 +52,20 @@ export const EventPage = () => {
         <p>End time: {end}</p>
         <p>Location: {event.location}</p>
         <br />
-        <p>Categories: fix category id</p>
+        {categoryList.length > 1 ? (
+          <p>Categories: {categoryList.join(" - ")} </p>
+        ) : (
+          <p>Category: {categoryList}</p>
+        )}
+        <UserCard userId={event.createdBy} />
       </CardBody>
 
       <CardFooter>
         <Button>Edit</Button>
         <Button>Delete</Button>
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
       </CardFooter>
     </Card>
   );
