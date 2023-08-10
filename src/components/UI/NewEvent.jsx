d366baa9ec0abf5a4ec545b25cae6a06f2af2170
McLizzerson <liesbeth.fr@gmail.com>
2023-08-10 12:41:48 +0200

    Added toast to submit NewEvent

--- src/components/UI/NewEvent.jsx
+++ src/components/UI/NewEvent.jsx
@@ -2,22 +2,25 @@ import { Flex, Button, Input, Stack, Select } from "@chakra-ui/react";
 import { Form, redirect } from "react-router-dom";
 import { useContext } from "react";
 import { EventsContext } from "../../Context";
+import { useToast } from "@chakra-ui/react";
 
 export const action = async ({ request }) => {
-  console.log("we're in action, going to try and post!");
   const formData = Object.fromEntries(await request.formData());
-  const newId = await fetch("http://localhost:3000/events", {
+  const response = await fetch("http://localhost:3000/events", {
     method: "POST",
     body: JSON.stringify(formData),
     headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .then((json) => json.id);
+  });
+
+  const json = await response.json();
+  const newId = await json.id;
+
   return redirect(`/event/${newId}`);
 };
 
 export const NewEvent = () => {
   const { users, categories } = useContext(EventsContext);
+  const toast = useToast();
 
   return (
     <Flex direction="column">
@@ -87,7 +90,19 @@ export const NewEvent = () => {
             })}
           </Select>
         </Stack>
-        <Button type="submit" variant="ghost">
+        <Button
+          type="submit"
+          variant="ghost"
+          onClick={() =>
+            toast({
+              title: "Event added",
+              description: "Your event was added succesfully!",
+              status: "success",
+              duration: 3000,
+              isClosable: true,
+            })
+          }
+        >
           Submit
         </Button>
       </Form>
