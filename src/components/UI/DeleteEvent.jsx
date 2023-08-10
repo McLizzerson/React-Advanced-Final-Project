dc45dadbac993ead0bd8429bbdeea1949d780c65
McLizzerson <liesbeth.fr@gmail.com>
2023-08-10 16:51:15 +0200

    Add toast and Link to DeleteEvent

--- src/components/UI/DeleteEvent.jsx
+++ src/components/UI/DeleteEvent.jsx
@@ -8,19 +8,54 @@ import {
   useDisclosure,
   Button,
 } from "@chakra-ui/react";
-import { redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
 
 export const DeleteEvent = ({ event }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  let toast = useToast();
 
-  const handleDelete = async () => {
+  // the action of deleting an event
+  const deleteEvent = async () => {
     const response = await fetch(`http://localhost:3000/events/${event.id}`, {
       method: "DELETE",
     });
+    const deletingStatus = response.status;
+    return deletingStatus;
+  };
 
-    console.log(response);
-    console.log(response.redirect);
-    return redirect(`./events`);
+  // handling the deletion and sending a toast with the response status
+  const handleDelete = async () => {
+    const status = await deleteEvent();
+
+    switch (status) {
+      case 200:
+        toast({
+          title: "Event deleted",
+          description: "Your event was deleted succesfully!",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+        break;
+      case 404:
+        toast({
+          title: "Oops",
+          description: `The event you tried to delete cannot be found ${status} `,
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        break;
+      default:
+        toast({
+          title: "Woah",
+          description: `Something happened! Not sure what "${status}" means though...`,
+          status: "warning",
+          duration: 3000,
+          isClosable: true,
+        });
+    }
   };
 
   return (
@@ -34,9 +69,11 @@ export const DeleteEvent = ({ event }) => {
           <ModalBody>Are you sure you want to delete this event?</ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="red" onClick={handleDelete}>
-              Delete
-            </Button>
+            <Link to="/">
+              <Button colorScheme="red" onClick={handleDelete}>
+                Delete
+              </Button>
+            </Link>
             <Button variant="ghost" mr={3} onClick={onClose}>
               Close
             </Button>
