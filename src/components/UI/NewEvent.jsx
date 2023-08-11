99cb4dd8424cd3cb76e62696c2836ece19ad6f6d
McLizzerson <liesbeth.fr@gmail.com>
2023-08-11 14:11:37 +0200

    Add toast to NewEvent

--- src/components/UI/NewEvent.jsx
+++ src/components/UI/NewEvent.jsx
@@ -1,7 +1,8 @@
 import { Flex, Button, Input, Stack, Select } from "@chakra-ui/react";
-import { Form, redirect } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 import { useContext } from "react";
 import { EventsContext } from "../../Context";
+import { useActionData } from "react-router-dom";
 import { useToast } from "@chakra-ui/react";
 
 export const action = async ({ request }) => {
@@ -15,12 +16,40 @@ export const action = async ({ request }) => {
   const json = await response.json();
   const newId = await json.id;
 
-  return redirect(`/event/${newId}`);
+  return {
+    status: response.status,
+    id: newId,
+  };
 };
 
 export const NewEvent = () => {
   const { users, categories } = useContext(EventsContext);
   const toast = useToast();
+
+  // using the data from the POST request to send a toast
+  const info = useActionData();
+  if (info !== undefined) {
+    const status = info.status;
+    switch (status) {
+      case 201:
+        toast({
+          title: "Success!",
+          description: `You're event was added succesfully`,
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+        break;
+      default:
+        toast({
+          title: "Woah",
+          description: `Something happened! Not sure what "${status}" means though...`,
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+        });
+    }
+  }
 
   return (
     <Flex direction="column">
@@ -90,22 +119,20 @@ export const NewEvent = () => {
             })}
           </Select>
         </Stack>
-        <Button
-          type="submit"
-          variant="ghost"
-          onClick={() =>
-            toast({
-              title: "Success!",
-              description: "Your event was added succesfully",
-              status: "success",
-              duration: 3000,
-              isClosable: true,
-            })
-          }
-        >
+        <Button type="submit" variant="ghost">
           Submit
         </Button>
       </Form>
+      {/* Adding a button to the page when an event has been added succesfully to navigate to the evenlist page */}
+      {info !== undefined && (
+        <>
+          <Link to={`/`}>
+            <Button color="green.500">
+              Check if your event is in the list!
+            </Button>
+          </Link>
+        </>
+      )}
     </Flex>
   );
 };
