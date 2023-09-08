a57caf92357ab11de995dc12ebc54a69452f997c
McLizzerson <liesbeth.fr@gmail.com>
2023-09-08 11:45:45 +0200

    Create theme for fonts

---
+++ src/components/Theme.jsx
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'pacifico', sans-serif`,
+    body: `'open-sans', sans-serif`,
+  },
+});
+
+export default theme;
