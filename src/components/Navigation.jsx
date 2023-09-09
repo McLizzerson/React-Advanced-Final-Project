2ec00286b3df588d6291dd8b3c6dbcc79a856cba
McLizzerson <liesbeth.fr@gmail.com>
2023-09-09 12:32:05 +0200

    Add icons and styling to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Flex, Heading, Input } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Events</Link>
-        </li>
-        <li>
-          <Link to="/event/1">Event</Link>
-        </li>
-      </ul>
-    </nav>
+    <Flex flexDirection="row" gap={4} position="fixed" top={0} padding={2}>
+      <Link to="/">
+        <Button>🏠</Button>
+      </Link>
+      <Link to="/new">
+        <Button>➕ new event</Button>
+      </Link>
+    </Flex>
   );
 };
