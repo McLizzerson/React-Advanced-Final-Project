da33cf96c5a5848cccb78c89e75f5623117eb5d0
McLizzerson <liesbeth.fr@gmail.com>
2023-09-09 18:20:33 +0200

    Fix toast repeat, add button, hide submit

--- src/components/UI/NewEvent.jsx
+++ src/components/UI/NewEvent.jsx
@@ -1,6 +1,16 @@
-import { Flex, Button, Input, Stack, Select } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Input,
+  Stack,
+  Select,
+  Card,
+  CardHeader,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import { Form, Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { EventsContext } from "../../Context";
 import { useActionData } from "react-router-dom";
 import { useToast } from "@chakra-ui/react";
@@ -23,116 +33,141 @@ export const action = async ({ request }) => {
 };
 
 export const NewEvent = () => {
+  const [sentToast, setSentToast] = useState(false);
   const { users, categories } = useContext(EventsContext);
   const toast = useToast();
 
-  // using the data from the POST request to send a toast
-  const info = useActionData();
-  if (info !== undefined) {
-    const status = info.status;
+  // use data from POST request to send toast and create id to redirect
+  const response = useActionData();
+  let id;
+  if (response != undefined) {
+    id = response.id;
+  }
+
+  if (response != undefined && sentToast === false) {
+    const status = response.status;
     switch (status) {
       case 201:
         toast({
           title: "Success!",
           description: `You're event was added succesfully`,
           status: "success",
-          duration: 5000,
+          duration: 1850,
           isClosable: true,
         });
+        setSentToast(true);
         break;
       default:
         toast({
           title: "Woah",
           description: `Something happened! Not sure what "${status}" means though...`,
           status: "warning",
-          duration: 5000,
+          duration: 1850,
           isClosable: true,
         });
+        setSentToast(true);
     }
   }
 
+  console.log(`response is: ${response}`);
+
   return (
-    <Flex direction="column">
-      Fill in the information about your event below.
-      <Form method="post" id="new-event-form">
-        <Stack spacing={3}>
-          <Input
-            placeholder="Event title"
-            type="text"
-            name="title"
-            required="required"
-          />
-          <Input
-            placeholder="Description"
-            type="text"
-            name="description"
-            required="required"
-          />
-          <Input
-            placeholder="Image url"
-            type="url"
-            name="image"
-            required="required"
-          />
-          <Input
-            type="datetime-local"
-            variant="outline"
-            placeholder="Start time"
-            name="startTime"
-            required="required"
-          />
-          <Input
-            type="datetime-local"
-            variant="outline"
-            placeholder="End time"
-            name="endTime"
-            required="required"
-          />
-          <Input
-            placeholder="Location"
-            type="text"
-            name="location"
-            required="required"
-          />
+    <Flex height="100vh" width="100vw" align="center" justify="center">
+      <Card gap={4} padding={4}>
+        <CardHeader>
+          <Heading>Create a new event 🎉</Heading>
+        </CardHeader>
 
-          <Select placeholder="Category" name="categoryIds" required="required">
-            {categories.map((category) => {
-              return (
-                <option value={category.id} key={category.id} type="number">
-                  {category.name}
-                </option>
-              );
-            })}
-          </Select>
+        {/* Adding a button to the page when an event has been added succesfully to navigate to the evenlist page */}
+        {id !== undefined && (
+          <Flex justify="center">
+            <Link to={`/event/${id}`}>
+              <Button size="lg" color="green.500">
+                Go to your Event!
+              </Button>
+            </Link>
+          </Flex>
+        )}
 
-          <Select
-            placeholder="Select user"
-            name="createdBy"
-            required="required"
-          >
-            {users.map((user) => {
-              return (
-                <option value={user.id} key={user.id} type="number">
-                  {user.name}
-                </option>
-              );
-            })}
-          </Select>
-        </Stack>
-        <Button type="submit" variant="ghost">
-          Submit
-        </Button>
-      </Form>
-      {/* Adding a button to the page when an event has been added succesfully to navigate to the evenlist page */}
-      {info !== undefined && (
-        <>
-          <Link to={`/`}>
-            <Button color="green.500">
-              Check if your event is in the list!
-            </Button>
-          </Link>
-        </>
-      )}
+        <Text align="center">
+          Fill in the information about your event below
+        </Text>
+        <Form method="post" id="new-event-form">
+          <Stack spacing={3}>
+            <Input
+              placeholder="Event title"
+              type="text"
+              name="title"
+              required="required"
+            />
+            <Input
+              placeholder="Description"
+              type="text"
+              name="description"
+              required="required"
+            />
+            <Input
+              placeholder="Image url"
+              type="url"
+              name="image"
+              required="required"
+            />
+            <Input
+              type="datetime-local"
+              variant="outline"
+              placeholder="Start time"
+              name="startTime"
+              required="required"
+            />
+            <Input
+              type="datetime-local"
+              variant="outline"
+              placeholder="End time"
+              name="endTime"
+              required="required"
+            />
+            <Input
+              placeholder="Location"
+              type="text"
+              name="location"
+              required="required"
+            />
+
+            <Select
+              placeholder="Category"
+              name="categoryIds"
+              required="required"
+            >
+              {categories.map((category) => {
+                return (
+                  <option value={category.id} key={category.id} type="number">
+                    {category.name}
+                  </option>
+                );
+              })}
+            </Select>
+
+            <Select
+              placeholder="Select user"
+              name="createdBy"
+              required="required"
+            >
+              {users.map((user) => {
+                return (
+                  <option value={user.id} key={user.id} type="number">
+                    {user.name}
+                  </option>
+                );
+              })}
+            </Select>
+            {response === undefined && (
+              <Button type="submit" variant="ghost">
+                Submit
+              </Button>
+            )}
+          </Stack>
+        </Form>
+      </Card>
     </Flex>
   );
 };
